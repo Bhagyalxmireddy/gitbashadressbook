d961b0a6db752c9ff2e21794fa27db2386453308
Bhagi <puppyreddy1997@gmail.com>
2020-10-04 21:34:44 +0530

    [Bhagyalaxmi]Added:editing the person details

--- AddressBook.java
+++ AddressBook.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 public class AddressBook
 {
+	static final int ADDRESS = 1, CITY = 2, STATE = 3, ZIP = 4, PHONENUMBER = 5;
 	public static List<ContactPerson> persons = new ArrayList<>();
 	static Scanner scan = new Scanner(System.in);
 	public void addPerson()
@@ -26,15 +27,102 @@ public class AddressBook
 		ContactPerson person1 = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber);
 		persons.add(person1);
 		System.out.println("person details added successfully \n");
-
-
-
-
 	}
+	public void printPersonDetails()
+	{
+        	if (persons.isEmpty())
+		{
+            		System.out.println("There are no contact to print ");
+        	}
+		else
+		{
+            		for (ContactPerson contact : persons)
+			{
+                		System.out.println(contact);
+            		}
+        	}
+    	}
+
+	public void editPersonDetails() 
+	{
+        	if (persons.isEmpty()) 
+		{
+        		System.out.println("There are no contacts to edit ");
+        	}
+		else
+		{
+        		String address, city, state;
+            		long phone, zip;
+            		int id;
+            		for (ContactPerson contact : persons)
+			{
+                		System.out.println("ID: #" + persons.indexOf(contact) + " : " + contact);
+            		}
+            			System.out.println(" Enter ID of contact to Edit : ");
+            			id = scan.nextInt();
+            			System.out.println(persons.get(id));
+            			System.out.println("please select the option to edit...\n \n 1. Address \n 2.city \n 3.state \n 4.zip \n 5.phone number ");
+            			int choice = scan.nextInt();
+            			switch (choice)
+				{
+                			case ADDRESS:
+                    				System.out.println("Enter Address: ");
+                    				address = scan.next();
+                    				persons.get(id).setAddress(address);
+					break;
+					case CITY:
+                 	   			System.out.println("Enter City");
+                    				city = scan.next();
+                    				persons.get(id).setCity(city);
+					break;
+					case STATE:
+                    				System.out.println("Enter State");
+                    				state = scan.next();
+                    				persons.get(id).setState(state);
+					case ZIP:
+                    				System.out.println("Enter Zip");
+                    				zip = scan.nextLong();
+                    				persons.get(id).setzip(zip);
+                    			break;
+                			case PHONENUMBER:
+                    				System.out.println("Enter Mobile number");
+                    				phone = scan.nextLong();
+                    				persons.get(id).setphoneNumber(phone);
+                    			break;
+                			default:
+                    				System.out.println("Please Enter Valid Option");
+                    				editPersonDetails();
+            			}
+        }
+    }
+
 	public static void main(String[] args)
 	{
+		int choice = 0;
 		AddressBook contact = new AddressBook(); 
 		System.out.println(" WelCome to Address Book Program ");
-		contact.addPerson();
+		while(choice < 4)
+		{
+			System.out.println("    Menu Details    ");
+			System.out.println("\n Enter your choice " +
+			"\n 1.Add persondetails " +
+			"\n 2.print persons details" +
+			"\n 3.Edit person details "+
+			"\n 4.Exit" );
+		choice = scan.nextInt();
+		switch(choice)
+		{
+			case 1:
+				contact.addPerson();
+			break;
+			case 2:
+				System.out.println("All the person details in the address book :::");
+				contact.printPersonDetails();
+			break;
+			case 3:
+				contact.editPersonDetails();
+			break;
+		}
+		}
 	}
 }
